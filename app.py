71d4e1e5e529e01c783dd069d554d607b3f26729
Hussianmk <hussianmk786@gmail.com>
2026-05-25 19:41:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -539,4 +539,4 @@ if __name__ == '__main__':
     print("  NCC Portal – Army Wing")
     print("  http://127.0.0.1:5000")
     print("─" * 50)
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)
